0be433cd78dded3f62d9cfcf96a055f8460adb05
Guilherme Dine <guilherme.dine@gmail.com>
2022-12-08 16:36:20 -0300

    NewAPI

---
+++ src/database/index.ts
@@ -0,0 +1,20 @@
+import { DataSource } from "typeorm";
+
+const PostgresDataSource = new DataSource({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "docker",
+  password: "ignite",
+  database: "rentx",
+  entities: [
+  ],
+})
+
+PostgresDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err)
+    })
